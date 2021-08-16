72edf4c5f4e33c95ee7355fe482ae220d3c18736
kyrylokharchenko <kharchenko.kir@gmail.com>
2021-08-16 17:57:26 +0300

    [-] added ui for future impementation.

---
+++ lib/api_mamager.dart
@@ -0,0 +1,17 @@
+class ApiManager {
+  Future<String> getHttp() {
+    return Future.value('s');
+  }
+
+  Future<String> postHttp() {
+    return Future.value('s');
+  }
+
+  Future<String> getChopper() {
+    return Future.value('s');
+  }
+
+  Future<String> postChopper() {
+    return Future.value('s');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:oktoast/oktoast.dart';
+import 'package:pet/api_mamager.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,39 +30,71 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final ApiManager apiManager = ApiManager();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return OKToast(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
+        body: Container(
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ElevatedButton(
+                      child: Text('HTTP GET'),
+                      onPressed: () {
+                        apiManager.getHttp().then((value) {
+                          showToast(value);
+                        });
+                      },
+                    ),
+                    ElevatedButton(
+                      child: Text('HTTP POST'),
+                      onPressed: () {
+                        apiManager.postHttp().then((value) {
+                          showToast(value);
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 12,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ElevatedButton(
+                      child: Text('Chopper GET'),
+                      onPressed: () {
+                        apiManager.getChopper().then((value) {
+                          showToast(value);
+                        });
+                      },
+                    ),
+                    ElevatedButton(
+                      child: Text('Chopper POST'),
+                      onPressed: () {
+                        apiManager.postChopper().then((value) {
+                          showToast(value);
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
